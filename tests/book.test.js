ed44ed383cc31fd784b1c9519d0293cae83b548e
rafael ubuntu <ryikeda@outlook.com>
2020-08-01 23:05:24 +0900

    added tests to POST /books

--- tests/book.test.js
+++ tests/book.test.js
@@ -53,3 +53,88 @@ describe("GET /books/:isbn", () => {
     expect(res.statusCode).toBe(404);
   });
 });
+
+// POST Route
+describe("POST /books", () => {
+  const testBook2 = {
+    isbn: "22222",
+    amazon_url: "https://amazon.com/testBook2",
+    author: "Tanaka",
+    language: "Japanese",
+    pages: 600,
+    publisher: "Japan Books",
+    title: "Tanaka book",
+    year: 2000,
+  };
+
+  test("Creates a new book", async () => {
+    const res = await request(app).post(`/books`).send(testBook2);
+    const book = res.body.book;
+    expect(res.statusCode).toBe(201);
+    expect(book).toHaveProperty("isbn");
+    expect(book.isbn).toEqual(testBook2.isbn);
+  });
+
+  test("Prevents creating a duplicated isbn", async () => {
+    const testDuplicateIsbn = { ...testBook2 };
+    testDuplicateIsbn.isbn = bookIsbn;
+    const res = await request(app).post(`/books`).send(testDuplicateIsbn);
+    expect(res.statusCode).toBe(400);
+  });
+
+  test("Prevents creating book without isbn", async () => {
+    const testNoIsbn = { ...testBook2 };
+    delete testNoIsbn.isbn;
+    const res = await request(app).post(`/books`).send(testNoIsbn);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error.error).toContain('instance requires property "isbn"');
+  });
+
+  test("Prevents creating book without author", async () => {
+    const testNoAuthor = { ...testBook2 };
+    delete testNoAuthor.author;
+    const res = await request(app).post(`/books`).send(testNoAuthor);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error.error).toContain(
+      'instance requires property "author"'
+    );
+  });
+
+  test("Prevents creating book without language", async () => {
+    const testNolanguage = { ...testBook2 };
+    delete testNolanguage.language;
+    const res = await request(app).post(`/books`).send(testNolanguage);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error.error).toContain(
+      'instance requires property "language"'
+    );
+  });
+
+  test("Prevents creating book without pages", async () => {
+    const testNopages = { ...testBook2 };
+    delete testNopages.pages;
+    const res = await request(app).post(`/books`).send(testNopages);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error.error).toContain(
+      'instance requires property "pages"'
+    );
+  });
+
+  test("Prevents creating book without title", async () => {
+    const testNotitle = { ...testBook2 };
+    delete testNotitle.title;
+    const res = await request(app).post(`/books`).send(testNotitle);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error.error).toContain(
+      'instance requires property "title"'
+    );
+  });
+
+  test("Prevents creating book without year", async () => {
+    const testNoyear = { ...testBook2 };
+    delete testNoyear.year;
+    const res = await request(app).post(`/books`).send(testNoyear);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error.error).toContain('instance requires property "year"');
+  });
+});
